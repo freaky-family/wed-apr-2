816206644866f435e0ea4cd1a7f8dfd2c7decce2
Nadal Berthelon <nadal.berthelon@epitech.eu>
2025-05-22 20:47:43 +0200

    [Wed Apr 2] Wed Apr 2

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 let array = [];
 let start = 0;
 let end = 10;
-let last_mon_mar = 0;
+let last_wed_apr = 0;
 
 const current_date = new Date();
 
-function get_next_mon_mar(x) {
+function get_next_wed_apr(x) {
     let date = current_date;
 
-    x += last_mon_mar;
+    x += last_wed_apr;
     while (date.getDay() !== 1) {
-        date = new Date(current_date.getFullYear() + x, 2, 3);
+        date = new Date(current_date.getFullYear() + x, 4, 2);
         x++;
     }
-    last_mon_mar = x;
+    last_wed_apr = x;
     array.push(date);
 }
 
-function loop_mon_mar() {
+function loop_wed_apr() {
     for (; start < end; start++) {
-        get_next_mon_mar(start + 1);
+        get_next_wed_apr(start + 1);
     }
 }
 
-loop_mon_mar();
+loop_wed_apr();
 
-const next_mon_mar = document.getElementById("next_mon_mar");
-next_mon_mar.innerText = array[0].toDateString();
+const next_wed_apr = document.getElementById("next_wed_apr");
+next_wed_apr.innerText = array[0].toDateString();
 
-const how_much_mon_mars = document.getElementById("how_much_mon_mars");
-how_much_mon_mars.innerText = array.length - 1;
+const how_much_wed_aprs = document.getElementById("how_much_wed_aprs");
+how_much_wed_aprs.innerText = array.length - 1;
 
-const mon_mars = document.getElementById("mon_mars");
+const wed_aprs = document.getElementById("wed_aprs");
 for (let i = 1; i < array.length; i++) {
     const date_element = document.createElement("h2");
     date_element.innerText = array[i].toDateString();
-    mon_mars.append(date_element);
+    wed_aprs.append(date_element);
 }
